55ed42a01e0e697ff91a0d0f444488ab581f2997
Jorge Barahona <bacj010509@gs.utm.mx>
2024-01-19 00:17:50 -0600

    break. Operador ternario.

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 let numeroSecreto = 4;
 let numeroUsuario = 0;
 let intentos = 1;
-let palabraVeces = 'vez';
+// let palabraVeces = 'vez';
+let maximosIntentos = 3;
 
 while (numeroUsuario != numeroSecreto) {
     let numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor:");
@@ -10,7 +11,7 @@ while (numeroUsuario != numeroSecreto) {
     console.log(numeroUsuario);
     if (numeroSecreto == numeroUsuario) {
         // Acertamos, fue verdadera la condición
-        alert(`Acertaste, el número secreto es: ${numeroSecreto}. Lo hiciste en ${intentos} ${palabraVeces}.`);
+        alert(`Acertaste, el número secreto es: ${numeroSecreto}. Lo hiciste en ${intentos} ${intentos == 1 ? 'vez' : 'veces'}.`);
     } else {
         if(numeroUsuario > numeroSecreto) {
             alert('El número secreto es menor');
@@ -18,8 +19,14 @@ while (numeroUsuario != numeroSecreto) {
             alert('El número secreto es mayor');
         }
         // Incrementamos el contador cuando no acierta
-        intentos = intentos + 1;
+        // intentos = intentos + 1;
+        // intentos += 1;
+        intentos++;
         palabraVeces = 'veces';
+        if (intentos > maximosIntentos) {
+            alert(`Llegaste al número máximo de ${maximosIntentos} intentos`);
+            break;
+        }
         // No acertamos, fue falsa la condición
         // alert("Lo siento, no has acertado el número.")
     }
