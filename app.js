44654303c6d76f30e92f6712e9a7c9c4e95a1294
Jorge Barahona <bacj010509@gs.utm.mx>
2024-01-18 23:01:11 -0600

    Comentarios. Sentencia else.

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
-javascript
-alert('Bienvenidos al juego del número secreto');
-let eleccion = prompt('Elige un número entre 1 y 10');
+// Variables
+let numeroSecreto = 6;
+let numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor:");
 
-let numeroSecreto = 4;
-
-if (eleccion == numeroSecreto) {
-    alert('Adivinaste');
-} 
+console.log(numeroUsuario);
+/*
+Este código se realiza
+la comparación
+*/
+if (numeroSecreto == numeroUsuario) {
+    // Acertamos, fue verdadera la condición
+  alert("Has acertado el número!");
+} else {
+    // No acertamos, fue falsa la condición
+  alert("Lo siento, no has acertado el número.")
+}
