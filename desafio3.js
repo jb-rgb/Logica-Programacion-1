9156de3bdfdc8842c00fd78f3e1611f660746294
Jorge Barahona <bacj010509@gs.utm.mx>
2024-01-19 00:04:42 -0600

    Desafio 3.

---
+++ desafio3.js
@@ -0,0 +1,36 @@
+// 1
+
+let conta = 1;
+
+while (conta <= 10) {
+    alert(conta);
+    conta++;
+}
+
+// 2
+
+let contado = 10;
+
+while (contado >= 0) {
+    alert(contado);
+    contado--;
+}
+
+// 3
+
+let numeroUsuario = prompt('Ingrese un número para la cuenta regresiva:');
+
+while (numeroUsuario > 0) {
+    console.log(numeroUsuario);
+    numeroUsuario--;
+}
+
+// 4
+
+let numeroUsser = prompt('Ingrese un número para la cuenta progresiva:');
+let contador = 0;
+
+while (contador <= numeroUsser) {
+    console.log(contador);
+    contador++;
+}
