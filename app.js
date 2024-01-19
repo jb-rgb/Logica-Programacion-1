9b1313ef12fbf584eb2eabbae822a4a106598378
Jorge Barahona <bacj010509@gs.utm.mx>
2024-01-19 00:36:45 -0600

    Desafio final. Fin del Curso.

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
-// Variables
-let numeroSecreto = Math.floor(Math.random() * 10) + 1;
+//Variables
+let numeroMaximoPosible = 100;
+let numeroSecreto = Math.floor(Math.random() * numeroMaximoPosible) + 1;
 let numeroUsuario = 0;
 let intentos = 1;
-// let palabraVeces = 'vez';
-let maximosIntentos = 3;
+//let palabraVeces = 'vez';
+let maximosIntentos = 10;
 
 while (numeroUsuario != numeroSecreto) {
-    let numeroUsuario = parseInt(prompt("Me indicas un número entre 1 y 10 por favor:"));
-
-    console.log(numeroUsuario);
-    if (numeroSecreto == numeroUsuario) {
-        // Acertamos, fue verdadera la condición
-        alert(`Acertaste, el número secreto es: ${numeroSecreto}. Lo hiciste en ${intentos} ${intentos == 1 ? 'vez' : 'veces'}.`);
+    numeroUsuario = parseInt(prompt(`Ingresa un número entre 1 y ${numeroMaximoPosible}:`));
+    if (numeroUsuario == numeroSecreto) {
+        //Acertamos, fue verdadera la condición
+        alert(`Acertaste, el número es: ${numeroUsuario}. Lo hiciste en ${intentos} ${intentos == 1 ? 'vez' : 'veces' }`);
     } else {
-        if(numeroUsuario > numeroSecreto) {
+        if (numeroUsuario > numeroSecreto) {
             alert('El número secreto es menor');
         } else {
             alert('El número secreto es mayor');
         }
-        // Incrementamos el contador cuando no acierta
-        // intentos = intentos + 1;
-        // intentos += 1;
+        //Incrementamos el contador cuando no acierta
+        //intentos = intentos + 1;
+        //intentos += 1;
         intentos++;
-        palabraVeces = 'veces';
+
+        //palabraVeces = 'veces';
         if (intentos > maximosIntentos) {
             alert(`Llegaste al número máximo de ${maximosIntentos} intentos`);
             break;
         }
-        // No acertamos, fue falsa la condición
-        // alert("Lo siento, no has acertado el número.")
+        //La condición no se cumplió
+        //alert('Lo siento, no acertaste el número');
     }
 }
