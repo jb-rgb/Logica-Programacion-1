1687662ce78c1c8ce02981d1f00d44aef6a94d00
Jorge Barahona <bacj010509@gs.utm.mx>
2024-01-19 00:29:34 -0600

    Número random.

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 // Variables
-let numeroSecreto = 4;
+let numeroSecreto = Math.floor(Math.random() * 10) + 1;
 let numeroUsuario = 0;
 let intentos = 1;
 // let palabraVeces = 'vez';
 let maximosIntentos = 3;
 
 while (numeroUsuario != numeroSecreto) {
-    let numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor:");
+    let numeroUsuario = parseInt(prompt("Me indicas un número entre 1 y 10 por favor:"));
 
     console.log(numeroUsuario);
     if (numeroSecreto == numeroUsuario) {
