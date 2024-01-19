0eb2298dd8242cfd61fe5a19386e18dde5cfe9ab
Jorge Barahona <bacj010509@gs.utm.mx>
2024-01-18 23:52:47 -0600

    Bucle while. Contador.

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 // Variables
-let numeroSecreto = 6;
-let numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor:");
+let numeroSecreto = 4;
+let numeroUsuario = 0;
+let intentos = 1;
+let palabraVeces = 'vez';
 
-console.log(numeroUsuario);
-/*
-Este código se realiza
-la comparación
-*/
-if (numeroSecreto == numeroUsuario) {
-    // Acertamos, fue verdadera la condición
-  alert(`Acertaste, el número secreto es: ${numeroSecreto}`);
-} else {
-    // No acertamos, fue falsa la condición
-  alert("Lo siento, no has acertado el número.")
+while (numeroUsuario != numeroSecreto) {
+    let numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor:");
+
+    console.log(numeroUsuario);
+    if (numeroSecreto == numeroUsuario) {
+        // Acertamos, fue verdadera la condición
+        alert(`Acertaste, el número secreto es: ${numeroSecreto}. Lo hiciste en ${intentos} ${palabraVeces}.`);
+    } else {
+        if(numeroUsuario > numeroSecreto) {
+            alert('El número secreto es menor');
+        } else {
+            alert('El número secreto es mayor');
+        }
+        // Incrementamos el contador cuando no acierta
+        intentos = intentos + 1;
+        palabraVeces = 'veces';
+        // No acertamos, fue falsa la condición
+        // alert("Lo siento, no has acertado el número.")
+    }
 }
